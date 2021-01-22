d5777565ba82be493d1969a3c24819ce3389bf57
filipp.karpov <filipp.karpov@nedra.digital>
2021-01-22 13:13:44 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import { MainPage } from "./pages/MainPage/MainPage";
 import { context } from "@reatom/react";
 import { App } from "./containers/App/App";
 import { store } from "./modules/app/app";
+import { EventInterceptorProvider, eventInterceptorMap } from "@consta/uikit/EventInterceptor";
 
 function Root() {
   return (
-    <context.Provider value={store}>
-      <App>
-        <MainPage />
-      </App>
-    </context.Provider>
+      <EventInterceptorProvider eventHandler={console.log} map={eventInterceptorMap}>
+          <context.Provider value={store}>
+              <App>
+                  <MainPage />
+              </App>
+          </context.Provider>
+      </EventInterceptorProvider>
   );
 }
 
